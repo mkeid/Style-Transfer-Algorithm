8b5fd2de528a1ea751e2904ba94e0ef95e5b1351
Mohamed K. Eid <mohamedkeid@gmail.com>
2017-01-03 18:11:16 -0500

    updated loss routines

--- style_transfer.py
+++ style_transfer.py
@@ -6,21 +6,22 @@ import custom_vgg19 as vgg19
 import numpy as np
 import tensorflow as tf
 import utils
+from functools import reduce
 from scipy.misc import toimage
 
 # Model Hyper Params
 content_layer = 'conv4_2'
 style_layers = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
 epochs = 1000
-learning_rate = .001
+learning_rate = .03
 total_variation_smoothing = 1.5
 norm_term = 6.
 
 # Loss term weights
-content_weight = .001
-style_weight = 1.
-norm_weight = 0
-tv_weight = 0
+content_weight = 1.
+style_weight = 100.
+norm_weight = 25.
+tv_weight = 50.
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -51,26 +52,27 @@ def convert_to_gram(filter_maps):
 # Compute the content loss given a variable image (x) and a content image (c)
 def get_content_loss(x, c):
     with tf.name_scope('get_content_loss'):
-        # Get the activated VGG feature maps and compute the mean-squared error
-        content_noise_out = getattr(x, content_layer)
-        content_photo_out = getattr(c, content_layer)
-        squared_errors = tf.square(content_noise_out - content_photo_out)
-        return tf.reduce_sum(squared_errors) / 2.
+        # Get the activated VGG feature maps and return the normalized euclidean distance
+        noise_representation = getattr(x, content_layer)
+        photo_representation = getattr(c, content_layer)
+        return get_l2_norm_loss(noise_representation - photo_representation)
+
+
+# Compute the L2-norm divided by squared number of dimensions
+def get_l2_norm_loss(diffs):
+    shape = diffs.get_shape().as_list()
+    size = reduce(lambda x, y: x * y, shape) ** 2
+    sum_of_squared_diffs = tf.reduce_sum(tf.square(diffs))
+    return sum_of_squared_diffs / size
 
 
 # Compute style loss given a variable image (x) and a style image (s)
 def get_style_loss(x, s):
     with tf.name_scope('get_style_loss'):
         style_layer_losses = [get_style_loss_for_layer(x, s, l) for l in style_layers]
-        style_weights = tf.constant([.2] * len(style_layer_losses), tf.float32)
+        style_weights = tf.constant([1. / len(style_layer_losses)] * len(style_layer_losses), tf.float32)
         weighted_layer_losses = tf.mul(style_weights, tf.convert_to_tensor(style_layer_losses))
         return tf.reduce_sum(weighted_layer_losses)
-
-
-# Compute the (p) norm of a given image (x)
-def get_norm(x, p):
-    summed_terms = tf.reduce_sum(tf.pow(x, p))
-    return tf.pow(summed_terms, 1. / p)
 
 
 # Compute style loss for a layer (l) given the variable image (x) and the style image (s)
@@ -85,14 +87,11 @@ def get_style_loss_for_layer(x, s, l):
         # Make sure the feature map dimensions are the same
         assert_equal_shapes = tf.assert_equal(x_layer_maps.get_shape(), s_layer_maps.get_shape())
         with tf.control_dependencies([assert_equal_shapes]):
-            # Compute the gram loss using the gram matrices
-            style_layer_shape = x_layer_maps.get_shape().as_list()
-            style_layer_elements = ((style_layer_shape[1] * style_layer_shape[2]) ** 2) * (style_layer_shape[3] ** 2)
-            style_fraction = 1. / (4 * style_layer_elements)
-            gram_loss = tf.reduce_sum(tf.square(x_layer_gram - s_layer_gram))
-
-            # Compute the end layer loss as the weighted gram loss
-            return style_fraction * gram_loss
+            # Compute and return the normalized gram loss using the gram matrices
+            shape = x_layer_maps.get_shape().as_list()
+            size = reduce(lambda a, b: a * b, shape) ** 2
+            gram_loss = get_l2_norm_loss(x_layer_gram - s_layer_gram)
+            return gram_loss / size
 
 
 # Compute total variation regularization loss term given a variable image (x) and its shape
@@ -101,13 +100,14 @@ def get_total_variation(x, shape):
         # Get the dimensions of the variable image
         height = shape[1]
         width = shape[2]
+        size = reduce(lambda a, b: a * b, shape) ** 2
 
         # Disjoin the variable image and evaluate the total variation
         x_cropped = x[:, :height - 1, :width - 1, :]
         left_term = tf.square(x[:, 1:, :width - 1, :] - x_cropped)
         right_term = tf.square(x[:, :height - 1, 1:, :] - x_cropped)
-        smoothed_summed_terms = tf.pow(tf.add(left_term, right_term), tf.div(total_variation_smoothing, 2))
-        return tf.pow(tf.reduce_sum(smoothed_summed_terms), 0.5)
+        smoothed_terms = tf.pow(left_term + right_term, total_variation_smoothing / 2.)
+        return tf.reduce_sum(smoothed_terms) / size
 
 
 # Render the generated image given a tensorflow session and a variable image (x)
@@ -167,7 +167,7 @@ with tf.Session() as sess:
         if norm_weight is 0:
             norm_loss = tf.constant(0.)
         else:
-            norm_loss = get_norm(noise, 2.) * norm_weight
+            norm_loss = (get_l2_norm_loss(noise) ** norm_term) * norm_weight
 
         # Total variation denoising
         if tv_weight is 0:
@@ -176,7 +176,7 @@ with tf.Session() as sess:
             tv_loss = get_total_variation(noise, image_shape) * tv_weight
 
         # Total loss
-        total_loss = tf.sqrt(content_loss + style_loss + norm_loss + tv_loss)
+        total_loss = content_loss + style_loss + norm_loss + tv_loss
 
     # Update image
     with tf.name_scope('update_image'):
@@ -189,8 +189,8 @@ with tf.Session() as sess:
     print("Initializing variables and beginning training..")
     sess.run(tf.initialize_all_variables())
     for i in range(epochs):
-        if i % 200 == 0:
-            print("Epoch %04d | Total Error is %.3f" % (i, sess.run(total_loss)))
+        if i % 250 == 0:
+            print("Epoch %04d | Total Error is %.4f" % (i, sess.run(total_loss)))
             render_img(sess, noise)
         sess.run(update_image)
 
